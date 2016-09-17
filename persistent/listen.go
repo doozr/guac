db43742d9b4b9250199eff8b9d204bb65575f752
Craig Andrews <craig@lazyrobot.org.uk>
2016-09-17 10:21:21 +0100

    Split send and receive goroutines so timeouts make sense

--- persistent/listen.go
+++ persistent/listen.go
@@ -13,11 +13,11 @@ import (
 //
 // Terminate by closing the `done` channel.
 func listen(r realtime.Connection, timeout time.Duration,
-	receiveChan chan []byte, sendChan chan asyncEvent,
-	done chan struct{}) {
+	receiveChan chan []byte, sendChan chan asyncEvent, done chan struct{}) {
 
 	wg := sync.WaitGroup{}
 	events := receive(r, done, &wg)
+	send(r, sendChan, done, &wg)
 
 	defer func() {
 		jot.Print("persistent.listen: shutting down")
@@ -37,15 +37,36 @@ func listen(r realtime.Connection, timeout time.Duration,
 		case event := <-events:
 			receiveChan <- event
 
-		case asyncEv := <-sendChan:
-			err := r.Send(asyncEv.event)
-			asyncEv.callback <- err
-
 		case <-time.After(timeout):
 			log.Printf("connection timeout after %s: reconnecting", timeout)
 			return
 		}
 	}
+}
+
+// send outgoing messages.
+func send(r realtime.Connection, sendChan chan asyncEvent,
+	done chan struct{}, wg *sync.WaitGroup) {
+	jot.Print("persistent.send: started")
+
+	wg.Add(1)
+	go func() {
+		defer func() {
+			wg.Done()
+			jot.Print("persistent.send: done")
+		}()
+
+		for {
+			select {
+			case <-done:
+				return
+
+			case asyncEv := <-sendChan:
+				err := r.Send(asyncEv.event)
+				asyncEv.callback <- err
+			}
+		}
+	}()
 }
 
 // receive incoming events from a connection and put them on a channel.
